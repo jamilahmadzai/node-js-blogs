6ec38cfd2c547a483509cfe0c55f3f552a36370d
Jamil Ur Rehman Ahmadzai <jamilurrehman722@gmail.com>
2021-02-17 03:18:09 +0100

    created schema and model for mongodb

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
+//express app
 const app = express();
 
+//connect to mongodb
+const dbURI =
+  "mongodb+srv://jamil:Jamil@mongodb@cluster0.tnrn4.mongodb.net/node-first-tutorial?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((error) => console.log(error));
+
+//register view engine
 app.set("view engine", "ejs");
 
-app.listen(3000);
+//middleware and static files
+app.use(express.static("public"));
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const blogs = [
